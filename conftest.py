09ba45c4ca310ad6176c4baba300bd49baf7533a
atansan <tommymadness@gmail.com>
2025-03-11 22:21:42 +0300

    homework for lesson #11

--- conftest.py
+++ conftest.py
@@ -1,35 +1,35 @@
-import os
-
 import pytest
-from dotenv import load_dotenv
-from selene import browser
+from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-@pytest.fixture(scope="session", autouse=True)
-def load_env():
-    load_dotenv()
 
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser():
+    browser.config.base_url = 'https://demoqa.com/automation-practice-form'
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options  # чтоб тест выполнялся когда сайт продолжается грузиться , но html загрузился
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    # driver_options = webdriver.ChromeOptions()   #настройка чтоб не открывать браузер , надо для этого 8 , 10 строчку кода
+    # driver_options.add_argument('--headless')
 
-@pytest.fixture(scope='function')
-def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": "125.0",
         "selenoid:options": {
             "enableVNC": True,
-            "enableVideo": True
+            "enableVideo": False
         }
     }
 
     options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+    browser.config.driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options)
 
-    browser.config.driver = driver
-    yield browser
+
+    yield
+    browser.quit()
