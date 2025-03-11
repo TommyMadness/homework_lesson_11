602534f15168e50dc5cb6682ac2fc998842f2bc4
atansan <tommymadness@gmail.com>
2025-03-11 20:08:42 +0300

    homework for lesson #11

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def browser_setup():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
